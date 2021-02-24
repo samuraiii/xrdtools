fd395d393926e6c106bcfa231518b411ca280b5a
Alexandr Mikula <mikula@fzu.cz>
2021-02-24 22:10:19 +0100

    xrd-drain.py moved to python3

--- xrd-drain.py
+++ xrd-drain.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
+# vim: set fileencoding=utf-8 :
 from os import path, readlink, remove, walk
 from re import escape, match, sub
 from subprocess import call
@@ -8,7 +9,7 @@ from hashlib import md5
 try:
     from multiprocessing import Queue, Lock, Pool, cpu_count
     mp = True
-except:
+except ModuleNotFoundError:
     print('No multiprocessing module found, parallel processing disabled.')
     mp = False
 
@@ -17,9 +18,10 @@ if not(len(argv) == 7) and not(len(argv) == 8):
     old_args = '\nYou gave:\n   ' + ' '.join(argv)
     argv[1:] = ['-h']
 
-if ( '-h' in argv ) or ( '--help' in argv ):
+if ('-h' in argv) or ('--help' in argv):
     print('This script is to be used in this way:')
-    print(argv[0] + ' /source/name/space/path /source/path [user@]destination.server[:port] /destination/name/space/path /destination/path user:group [number of threads]' + old_args)
+    print(argv[0] + ' /source/name/space/path /source/path [user@]destination.server[:port]'
+                    ' /destination/name/space/path /destination/path user:group [number of threads]' + old_args)
     exit(0)
 
 s_ns = argv[1]
@@ -31,8 +33,8 @@ dest = argv[5]
 fileog = argv[6]
 dtransfers = 1
 illegals = []
-sync_dirs_only = [ '--include=*/', '--exclude=*' ]
-usermatch = '[a-z][a-z0-9\\\-]+'
+sync_dirs_only = ['--include=*/', '--exclude=*']
+usermatch = r'[a-z][a-z0-9\\\-]+'
 
 if len(argv) == 8:
     mp_threads = int(argv[7])
@@ -72,32 +74,35 @@ sshf = ' ' + ' '.join(ssh) + ' '
 
 
 class noop(object):
+    """Does nothing by design"""
     def __init__(*args):
         pass
+
     def __enter__(*args):
         pass
+
     def __exit__(*args):
         pass
 
 
-def cdnf( cdir ):
-    'This check if parameter is existing dir or it fails script'
+def cdnf(cdir):
+    """This check if parameter is existing dir or it fails script"""
     if not path.isdir(cdir):
         exit(cdir + ' should be directory but it is not!')
     return
 
 
-def flatten(S):
-    'Flattens list recursively'
-    if S == []:
-        return S
-    if isinstance(S[0], list):
-        return flatten(S[0]) + flatten(S[1:])
-    return S[:1] + flatten(S[1:])
+def flatten(s):
+    """Flattens list recursively"""
+    if s == []:
+        return s
+    if isinstance(s[0], list):
+        return flatten(s[0]) + flatten(s[1:])
+    return s[:1] + flatten(s[1:])
 
 
-def explode( filestring, start='/'):
-    'Returns all dirs on a way from "start" to last dir (like in dirname)'
+def explode(filestring, start='/'):
+    """Returns all dirs on a way from "start" to last dir (like in dirname)"""
     members = []
     while not match(start + '/*$', filestring + '/'):
         members.append(filestring)
@@ -105,35 +110,36 @@ def explode( filestring, start='/'):
     return members
 
 
-def rds ( string ):
-    'Removes // from string'
-    return sub('//', '/', string )
+def rds(string):
+    """Removes // from string"""
+    return sub('//', '/', string)
 
 
-def rsync( cmd ):
-    'Call rsync with given arguments (Pass only options to this function)'
-    # Use lighter (arc4) encryption and no Compression to speed transfers up
-    cmd = flatten([ '/usr/bin/rsync', '-a', '-e', sshf, cmd ])
+def rsync(cmd):
+    """Call rsync with given arguments (Pass only options to this function)"""
+    cmd = flatten(['/usr/bin/rsync', '-a', '-e', sshf, cmd])
     return call(cmd)
 
 
-def migrate( lin, fil, m_transfers, ilock ):
-    'This migrates data and link to new destination'
+def migrate(lin, fil, m_transfers, ilock):
+    """""'This migrates data and link to new destination"""
     # Create destination 'addresses'
     d_file = rds(sub(escape(src), dest + '/', fil))
     d_link = rds(sub(escape(s_ns), d_ns + '/', lin))
-    cmd = [ rds(fil), rds(d_server + ':/' + d_file)]
+    cmd = [rds(fil), rds(d_server + ':/' + d_file)]
     d_filedir = path.dirname(d_file)
     d_linkdir = path.dirname(d_link)
     # get all dest dirs up to d_ns and dest
-    d_linkdir_members = ' '.join( explode(d_linkdir, d_ns))
-    d_filedir_members = ' '.join( explode(d_filedir, dest))
+    d_linkdir_members = ' '.join(explode(d_linkdir, d_ns))
+    d_filedir_members = ' '.join(explode(d_filedir, dest))
     # create directory structure on destination
-    if call(flatten([ ssh, d_server, '/bin/mkdir -p ' + d_filedir + ' ' + d_linkdir ])) == 0:
+    if call(flatten([ssh, d_server, '/bin/mkdir -p ' + d_filedir + ' ' + d_linkdir])) == 0:
         # Rsync data file
         if rsync(cmd) == 0:
             # Create link on destination and set owner:group
-            if call(flatten([ ssh, d_server, '/bin/ln -sf ' + d_file + ' ' + d_link + ' && /bin/chown -h ' + fileog + ' ' + d_link + ' ' + d_filedir_members + ' ' + d_linkdir_members])) == 0:
+            if call(flatten([ssh, d_server, '/bin/ln -sf ' + d_file + ' ' + d_link + ' && /bin/chown -h '
+                                            + fileog + ' ' + d_link + ' ' + d_filedir_members
+                                            + ' ' + d_linkdir_members])) == 0:
                 # Remove source data
                 remove(lin)
                 remove(fil)
@@ -143,13 +149,13 @@ def migrate( lin, fil, m_transfers, ilock ):
                 with ilock:
                     # Link failure
                     print('Failed to create link ' + d_server + ':' + d_link + ' or to set permissions!')
-        else :
+        else:
             with ilock:
                 # Data failure
                 print('Failed to copy file: ' + fil + ' to: ' + d_server + ':' + d_file)
-    else :
+    else:
         with ilock:
-            print('Failed to create directories: ' + d_filedir + ' and/or ' + d_linkdir )
+            print('Failed to create directories: ' + d_filedir + ' and/or ' + d_linkdir)
 
 
 def mp_process(mp_queue, mp_iolock):
@@ -157,12 +163,12 @@ def mp_process(mp_queue, mp_iolock):
         mp_link, mp_file, mp_number = mp_queue.get()
         if mp_link is None or mp_file is None or mp_number is None:
             break
-        migrate( mp_link, mp_file, mp_number, mp_iolock )
+        migrate(mp_link, mp_file, mp_number, mp_iolock)
 
 
-def clean_empty_dirs( d ):
-    'Finds (depth first) all empty dirs and deletes them'
-    call(['/bin/find', d, '-mindepth', '1', '-type', 'd', '-empty', '-delete' ])
+def clean_empty_dirs(directory_to_clean):
+    """Finds (depth first) all empty dirs and deletes them"""
+    call(['/bin/find', directory_to_clean, '-mindepth', '1', '-type', 'd', '-empty', '-delete'])
 
 
 if __name__ == '__main__':
@@ -171,18 +177,22 @@ if __name__ == '__main__':
     cdnf(src)
 
     testfile = '/.xrd-drain-testfile_55c4e792761ddeb2dca627ffadca546f82359' + src_id
-    testfile = [ d_ns + testfile, dest + testfile ]
-    try:
-        for f in testfile:
-            call(flatten([ ssh, d_server, '/bin/touch ' + f + ' && /bin/chown ' + fileog + ' ' + f + ' && /bin/rm -f ' + f ]))
-    except:
-        exit('Writing of testfiles to ' + d_ns + ' and ' + dest + ' failed!\n Is ' + fileog + ' defined on ' + d_server + '?')
+    testfile = [d_ns + testfile, dest + testfile]
+    for f in testfile:
+        returncode = call(flatten([ssh, d_server, '/bin/touch ' + f + ' && /bin/chown ' + fileog
+                                   + ' ' + f + ' && /bin/rm -f ' + f]))
+        if returncode != 0:
+            exit('Writing of testfiles to ' + d_ns + ' and '
+                 + dest + ' failed!\n Is ' + fileog + ' defined on ' + d_server + '?')
 
     if mp:
+        # Setup the multiprocess pool and queue
         m_queue = Queue(maxsize=mp_threads)
         iolock = Lock()
         pool = Pool(mp_threads, initializer=mp_process, initargs=(m_queue, iolock))
     else:
+        m_queue = None
+        pool = None
         iolock = noop()
     # Find all valid links and corresponding files
     for root, dirs, files in walk(s_ns, topdown=False):
@@ -215,12 +225,13 @@ if __name__ == '__main__':
                 # Add to illegal files if file is not a link
                 illegals.append(filepath)
     if mp:
+        # Finish and close queues
         for _ in range(mp_threads):
             m_queue.put((None, None, None))
         pool.close()
         pool.join()
 
-    #Count all illegals
+    # Count all illegals
     icount = len(illegals)
     if icount > 0:
         d = ''
@@ -241,7 +252,7 @@ if __name__ == '__main__':
             elif d == 'Q' or d == 'q':
                 break
             else:
-                print('Unknown choice "' + d +'"!')
+                print('Unknown choice "' + d + '"!')
 
     # Clean all empty dirs in src and s_ns
     print('Cleaning empty directories in ' + src + ' and ' + s_ns)
