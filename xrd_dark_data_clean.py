0a287d0fbdaa3c16298e5d5cbaf61d31b279982a
samuraiii <samurai.no.dojo@gmail.com>
2023-02-16 17:23:47 +0100

    multithreaded dark data cleaning
    Signed-off-by: samuraiii <samurai.no.dojo@gmail.com>

--- xrd_dark_data_clean.py
+++ xrd_dark_data_clean.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # vim: set fileencoding=utf-8 :
-# Version 1.2.1
+# Version 2.0.0
 '''
 Scans all entries in XRootD namespace
 and than scans all data files,
@@ -8,11 +8,107 @@ deleting all data files not found in namespace.
 It also finds all illegal namespace entires (not links)
 and allows them to be deleted.
 '''
-from os import path, readlink, remove, walk
-from sys import argv, exit, stdout  # pylint: disable=redefined-builtin
+from os import path, readlink, remove, cpu_count, scandir
+from sys import argv, exit, stdout, stderr # pylint: disable=redefined-builtin
 from subprocess import call
+from threading import Lock, Condition, Thread
 
-TEXT_WIDTH: int = 120
+TEXT_WIDTH: int = 150
+THREADS: int = cpu_count()*2
+
+def status_print(print_line: str, keep: bool = True, error: bool = False) -> None:
+    '''
+    Prints line TEXT_WIDTH long (left padded),
+    either permanently (keep == True) or to be overwritten.
+    '''
+    if error:
+        outfile = stderr
+    else:
+        outfile = stdout
+    formated_line: str = print_line.ljust(TEXT_WIDTH)
+    if keep:
+        print(formated_line, file = outfile)
+    else:
+        stdout.write(f'{formated_line}\r')
+        stdout.flush()
+
+
+def walk(top: str,
+    threads: int = THREADS,
+    separate_links: bool = True,
+    full_paths: bool = True):
+    '''
+    Multi-threaded version of os.walk() with some improvements
+    '''
+    if not path.isdir(top):
+        return
+    lock: Lock = Lock()
+    on_input: Condition = Condition(lock)
+    on_output: Condition = Condition(lock)
+    state: dict = {'tasks': 1}
+    paths: set = {top}
+    output: list = []
+
+    def worker() -> None:
+        while True:
+            with lock:
+                while True:
+                    if not state['tasks']:
+                        output.append(None)
+                        on_output.notify()
+                        return
+                    if not paths:
+                        on_input.wait()
+                        continue
+                    fs_path: str = paths.pop()
+                    break
+            try:
+                dirs: set = set()
+                files: set = set()
+                links: set = set()
+                for item in scandir(fs_path):
+                    item_name: str = item.name
+                    if full_paths:
+                        item_name = item.path
+                    if item.is_symlink():
+                        if separate_links:
+                            links.add(item_name)
+                        else:
+                            files.add(item_name)
+                    elif item.is_file():
+                        files.add(item_name)
+                    else:
+                        dirs.add(item_name)
+                        with lock:
+                            state['tasks'] += 1
+                            paths.add(item.path)
+                            on_input.notify()
+                with lock:
+                    if separate_links:
+                        output.append((fs_path, dirs, files, links))
+                    else:
+                        output.append((fs_path, dirs, files))
+                    on_output.notify()
+            except OSError as exception:
+                status_print(exception, error=True)
+            finally:
+                with lock:
+                    state['tasks'] -= 1
+                    if not state['tasks']:
+                        on_input.notify_all()
+
+    workers: list = [Thread(target=worker, name=f'fastio.walk {i} {top}') for i in range(threads)]
+    for start_worker in workers:
+        start_worker.start()
+    while threads or output:  # TODO(jart): Why is 'or output' necessary?
+        with lock:
+            while not output:
+                on_output.wait()
+            item: tuple = output.pop()
+        if item:
+            yield item
+        else:
+            threads -= 1
 
 
 def check_if_dir_exists(check_dir: str) -> None:
@@ -23,25 +119,142 @@ def check_if_dir_exists(check_dir: str) -> None:
         exit(f'{check_dir} should be directory but it is not!')
 
 
-def status_print(print_line: str, keep: bool=True) -> None:
-    '''
-    Prints line TEXT_WIDTH long (left padded),
-    either permanently (keep == True) or to be overwritten.
-    '''
-    formated_line: str = print_line.ljust(TEXT_WIDTH)
-    if keep:
-        print(formated_line)
-    else:
-        stdout.write(f'{formated_line}\r')
-        stdout.flush()
-
-
 def clean_empty_dirs(directory_to_be_cleaned: str) -> None:
     '''
     Finds (depth first) all empty dirs and deletes them
     '''
     call(['/bin/find', directory_to_be_cleaned, '-mindepth', \
         '1', '-type', 'd', '-empty', '-delete'])
+
+def get_name_space_links(name_space_root: str) -> tuple:
+    '''
+    Process the links in multithreaded way
+    '''
+    getns_lock: Lock = Lock()
+    getns_on_input: Condition = Condition(getns_lock)
+    getns_on_output: Condition = Condition(getns_lock)
+    getns_state: dict = {
+        'iterator': 0,
+        'link_count': 0,
+        'links_to_process': set(),
+        'illegal_ns_data': set(),
+        'valid_links': set(),
+    }
+
+    def process_name_space_link(link_to_check: str)-> str:
+        '''
+        Checks if given path is link to existing file
+        '''
+        out: str = link_to_check
+        link_target: str = readlink(link_to_check)
+        # Check if link address is absolute
+        if not path.isabs(link_target):
+            # Create absolute link address
+            link_target = path.abspath(path.join(path.dirname(link_target), link_target))
+        if path.isfile(link_target):
+            out = link_target
+        return out
+
+    # Find all link targets in ns
+    for _, _, name_space_files, name_space_links in walk(name_space_root):
+        for name_space_link in name_space_links:
+            getns_state['links_to_process'].add(name_space_link)
+            getns_state['link_count'] += 1
+            if (getns_state['link_count'] % 1_000) == 0:
+                status_print(f'Found NS entry {getns_state["link_count"]:_}: {name_space_link}', \
+                    keep=False)
+        for getns_illegal in name_space_files:
+            getns_state['illegal_ns_data'].add(getns_illegal)
+    status_print(f'Found {getns_state["link_count"]:_} name space entries')
+
+    def getns_worker() -> None:
+        while True:
+            with getns_lock:
+                while True:
+                    if len(getns_state['links_to_process']) == 0:
+                        getns_on_output.notify()
+                        return
+                    getns_path: str = getns_state['links_to_process'].pop()
+                    getns_state['iterator'] += 1
+                    if (getns_state['iterator'] % 1_000) == 0:
+                        status_print(f'Processing NS entry {getns_state["iterator"]:_} of '\
+                            f'{getns_state["link_count"]:_}: {getns_path}', keep=False)
+                    break
+            getns_target: str = process_name_space_link(getns_path)
+            with getns_lock:
+                if getns_target == getns_path:
+                    getns_state['illegal_ns_data'].add(getns_path)
+                else:
+                    getns_state['valid_links'].add(getns_target)
+                if len(getns_state['links_to_process']) == 0:
+                    getns_on_input.notify_all()
+
+    getns_workers: list = [Thread(target=getns_worker, \
+        name=f'getns_worker {i} {name_space_root}') for i in range(THREADS)]
+    for getns_start_worker in getns_workers:
+        getns_start_worker.start()
+    for getns_start_worker in getns_workers:
+        getns_start_worker.join()
+    status_print(f'Found {len(getns_state["valid_links"]):_} valid name space entries')
+    return getns_state['valid_links'], getns_state['illegal_ns_data']
+
+
+def find_dark_data(data_directories: list, name_space_links: set) -> set:
+    '''
+    Checks the data dirs for dark data files
+    '''
+    dark_data: set = set()
+    dark_iterator: int = 1
+    for data_dir in data_directories:
+        for _, _, data_files, data_links in walk(data_dir):
+            for data_link in data_links:
+                dark_data.add(data_link)
+            for data_file in data_files:
+                # Create file name
+                if (dark_iterator % 1_000) == 0:
+                    status_print(f'Processing data entry {dark_iterator:_}: {data_file}', \
+                        keep=False)
+                # Check if file is in NS entries
+                if not data_file in name_space_links:
+                    status_print(f'Found dark data file: {data_file}')
+                    dark_data.add(data_file)
+                dark_iterator += 1
+    return dark_data
+
+
+def delete(del_file) -> None:
+    '''
+    Try to delete file, fail silently if it does not exist
+    '''
+    try:
+        remove(del_file)
+    except FileNotFoundError:
+        pass
+
+
+def save_to_file(data: any) -> None:
+    '''
+    Saves the data to specified file
+    '''
+    savefile: str = ''
+    while True:
+        savefile = input('Please write file path for saving the data:\n')
+        if not path.isabs(savefile):
+            status_print('Please specify save file as absolute path:')
+        else:
+            savedir: str = path.dirname(savefile)
+            if not path.exists(savedir):
+                status_print(f'The directory {savedir} does not exist, '\
+                    'please select a different file path!')
+            else:
+                break
+    if isinstance(data, (list, set)):
+        data_out: str = '\n'.join(data)
+    else:
+        data_out = data
+    with open(savefile, 'w', encoding='utf-8') as savefile_handle:
+        savefile_handle.write(data_out)
+
 
 
 if __name__ == '__main__':
@@ -51,8 +264,8 @@ if __name__ == '__main__':
         argv[1:] = ['-h']
 
     if ('-h' in argv) or ('--help' in argv):
-        print('This script is to be used in this way:')
-        print(f'{argv[0]} /name/space/path /data/path/1 '\
+        status_print('This script is to be used in this way:')
+        status_print(f'{argv[0]} /name/space/path /data/path/1 '\
             '[/data/path/2] ... [/data/path/n] {OLD_ARGS}')
         exit(0)
 
@@ -60,83 +273,63 @@ if __name__ == '__main__':
     DATA_DIRS: list = argv[2:]
     NAME_SPACE_LINKS: set = set()
     ILLEGAL_NAME_SPACE_ENTRIES: set = set()
-    ITERATOR: int = 1
-    # Check if name space and source are dirs
+    DARK_DATA: set = set()
     check_if_dir_exists(NAME_SPACE)
     for DATA_DIR in DATA_DIRS:
         check_if_dir_exists(DATA_DIR)
 
-    print('Collecting all link targets in ' + NAME_SPACE)
+    status_print(f'Collecting all link targets in {NAME_SPACE}')
 
-    # Find all link targets in ns
-    for name_space_root, _, name_space_files in walk(NAME_SPACE):
-        for name_space_entry in name_space_files:
-            # Create file name
-            name_space_entry_path: str = path.join(name_space_root, name_space_entry)
-            if (ITERATOR % 1000) == 0:
-                status_print(f'Processing NS entry {ITERATOR}: {name_space_entry_path}', keep=False)
-            # Check if it is link
-            if path.islink(name_space_entry_path):
-                # Get link target
-                name_space_link_target: str = readlink(name_space_entry_path)
-                # Check if link address is absolute
-                if not path.isabs(name_space_link_target):
-                    # Create absolute link address
-                    name_space_link_target = path.abspath(
-                        path.join(
-                            path.dirname(name_space_link_target),
-                            name_space_link_target))
-                if path.isfile(name_space_link_target):
-                    NAME_SPACE_LINKS.add(name_space_link_target)
-                    ITERATOR += 1
-                else:
-                    ILLEGAL_NAME_SPACE_ENTRIES.add(name_space_entry_path)
+    NAME_SPACE_LINKS, ILLEGAL_NAME_SPACE_ENTRIES = get_name_space_links(NAME_SPACE)
+
+    DARK_DATA = find_dark_data(DATA_DIRS, NAME_SPACE_LINKS)
+
+    DARK_DATA_COUNT: int = len(DARK_DATA)
+    if DARK_DATA_COUNT > 0:
+        USER_INPUT: str = ''
+        while USER_INPUT not in {'q', 'Q', 'd', 'D'}:
+            status_print(f'Found {DARK_DATA_COUNT:_} dark data files.')
+            status_print('What would you like to do about it?')
+            USER_INPUT = input('(D)elete entires\n(L)ist entires\n'\
+                '(Q)uit and do nothing about it\n(S)ave the entirs into a file\n')
+            if USER_INPUT in {'D', 'd'}:
+                for dark_data_file in DARK_DATA:
+                    delete(dark_data_file)
+                status_print('Dark data files were deleted.')
+                USER_INPUT = 'Q'
+            elif USER_INPUT in {'L', 'l'}:
+                for dark_file in DARK_DATA:
+                    status_print(dark_file)
+            elif USER_INPUT in {'S', 's'}:
+                save_to_file(DARK_DATA)
             else:
-                ILLEGAL_NAME_SPACE_ENTRIES.add(name_space_entry_path)
-    status_print(f'Found total of {ITERATOR} links.')
+                status_print(f'Unknown choice "{USER_INPUT}"!')
 
-    ITERATOR = 1
-    for DATA_DIR in DATA_DIRS:
-        for data_root, _, data_files in walk(DATA_DIR):
-            for data_file in data_files:
-                # Create file name
-                data_file_path: str = path.join(data_root, data_file)
-                if (ITERATOR % 100) == 0:
-                    status_print(f'Processing data entry {ITERATOR}: {data_file_path}', keep=False)
-                # Check if file is in NS entries
-                if not data_file_path in NAME_SPACE_LINKS:
-                    status_print(f'Removing dark data file: {data_file_path}')
-                    remove(data_file_path)
-                ITERATOR += 1
 
-    # Count all illegals
     ILLEGALS_COUNT: int = len(ILLEGAL_NAME_SPACE_ENTRIES)
     if ILLEGALS_COUNT > 0:
         USER_INPUT: str = ''
-        # Ask what to do about all illegal files
-        # Ignore it with 'q'
         while USER_INPUT not in {'q', 'Q', 'd', 'D'}:
-            print(f'Found {ILLEGALS_COUNT} illegal (not links) entries in namespace.')
-            print('What would you like to do about it?')
-            print('\nBeware if you have some file systems unmounted!!!')
-            USER_INPUT = input('(D)elete entires\n(L)ist entires\n(Q)uit and do nothing about it\n')
-            # Delete illegals
+            status_print(f'Found {ILLEGALS_COUNT:_} illegal (not links or dangling links) '\
+                'entries in namespace.')
+            status_print('What would you like to do about it?')
+            status_print('\nBeware if you have some file systems unmounted!!!')
+            USER_INPUT = input('(D)elete entires\n(L)ist entires\n'\
+                '(Q)uit and do nothing about it\n(S)ave the entirs into a file\n')
             if USER_INPUT in {'D', 'd'}:
                 for ILLEGAL_ENTRY in ILLEGAL_NAME_SPACE_ENTRIES:
-                    try:
-                        remove(ILLEGAL_ENTRY)
-                    except FileNotFoundError:
-                        pass
-                print('Illegal entries were deleted.')
-                USER_INPUT = 'Q'
-            # List all illegal files
+                    delete(ILLEGAL_ENTRY)
+                status_print('Illegal entries were deleted.')
             elif USER_INPUT in {'L', 'l'}:
-                print(ILLEGAL_NAME_SPACE_ENTRIES)
+                for illegal in ILLEGAL_NAME_SPACE_ENTRIES:
+                    status_print(illegal)
+            elif USER_INPUT in {'S', 's'}:
+                save_to_file(ILLEGAL_NAME_SPACE_ENTRIES)
             else:
-                print(f'Unknown choice "{USER_INPUT}"!')
+                status_print(f'Unknown choice "{USER_INPUT}"!')
 
     # Clean all empty dirs in src and s_ns
-    print('Cleaning empty directories in data dirs')
+    status_print('Cleaning empty directories in data dirs')
     for DATA_DIR in DATA_DIRS:
         clean_empty_dirs(DATA_DIR)
     exit(0)
